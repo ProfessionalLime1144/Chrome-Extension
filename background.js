3f3851fa4da3f1f13b33139179bd9cdbdb45f501
Mohammed Rahman 1032 <rahmanmohammed1032@gmail.com>
2023-03-22 20:19:53 -0400

    Code has been optimized to return exact search results from the Brave browser

--- background.js
+++ background.js
@@ -5,9 +5,11 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message == "urlMatch") {
-      let search = sender.tab.url;
-      search = search.split("q=")[1];
-      search = search.split('&source=web')[0];
+      // Return the search data
+      let search = sender.tab.title;
+      // Remove the last 16 characters of string (which returns " - Brave Search")
+      search = search.slice(0, -15)
+      // search = search.split('&source=web')[0];
       sendResponse({ search });
     }
   }
