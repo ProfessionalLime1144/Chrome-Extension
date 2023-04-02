2b3f7aa9d2187793db28e9fd79d0f4e5359e0ba8
Mohammed Rahman 1032 <rahmanmohammed1032@gmail.com>
2023-04-01 21:27:33 -0400

    Moved the openai module info into contentScript and was able to debug errors

--- src/background.js
+++ src/background.js
@@ -1,33 +1,15 @@
-import apiKey from "../config/config.mjs";
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: apiKey
-})
-const openai = new OpenAIApi(configuration);
-async function ChatGptResponse(prompt) {
-  const res = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-  })
-  return res.data.choices[0].message.content;
-}
-
-
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Installed");
 });
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  async function(request, sender, sendResponse) {
     if (request.message === "urlMatch") {
       // Return the search data
       let search = sender.tab.title;
       // Remove the last 16 characters of string (which returns " - Brave Search")
       search = search.slice(0, -15);
-      sendResponse({
-        apiResponse: apiKey
-      });
+      sendResponse({ search });
     }
   }
 );
--- src/contentScript.js
+++ src/contentScript.js
@@ -1,11 +1,29 @@
-// import apiKey  from "../config/config.mjs";
-console.log("Content script loaded");
-// chrome.runtime.sendMessage(
-//   // This message is sent to background.js when one of the URL's in manifest.json is invoked.
-//   { message: "urlMatch"},
-//   async function(response) {
-//     // This callback is invoked when the background.js responds with a URL.
-//     const prompt = await response.apiResponse;
-//     console.log(response)
-//   }
-// );
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+  timeout: 60000 // 60 seconds
+});
+const openai = new OpenAIApi(configuration);
+async function ChatGptResponse(prompt) {
+  try {
+    const res = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+    return res.data.choices[0].message.content;
+  } catch (err) {
+    console.error("Failed to get OpenAI API response:", err);
+  }
+}
+
+chrome.runtime.sendMessage(
+  // This message is sent to background.js when one of the URL's in manifest.json is invoked.
+  { message: "urlMatch"},
+  async function(response) {
+    // This callback is invoked when the background.js responds with a URL.
+    const prompt = await response.search;
+    const apiResponse = await ChatGptResponse(prompt);
+    console.log(apiResponse);
+  } 
+);
